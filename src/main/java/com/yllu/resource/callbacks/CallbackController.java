3927dccb778a30944e37f9dbf2a1cb25efec3e6a
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2023-04-08 21:05:58 +0300

    Remove unused path

--- src/main/java/com/yllu/resource/callbacks/CallbackController.java
+++ src/main/java/com/yllu/resource/callbacks/CallbackController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/SALTEDGE/callback")
+@RequestMapping("/callback")
 @AllArgsConstructor
 public class CallbackController {
 
